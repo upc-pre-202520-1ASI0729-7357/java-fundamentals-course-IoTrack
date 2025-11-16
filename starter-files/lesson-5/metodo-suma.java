e935bf623b24e1c7124834f18b502cb2d810227a
nOOmzzzz <123845160+nOOmzzzz@users.noreply.github.com>
2025-11-16 03:19:29 -0500

    Add metodo-suma.java for basic sum operation

---
+++ starter-files/lesson-5/metodo-suma.java
@@ -0,0 +1,17 @@
+//Bienvenidos al curso de metodos en JAVA
+//metodo sumar() = cuatro
+//metodo restar()
+//metodo multipicar()
+//metodo dividir()
+//metodo int, void, string
+public class Main {
+    public static int sumar(int a, int b){
+        return a+b;
+    }
+    
+    public static void main(String[] args){
+        int resultado = sumar(5,3);
+        System.out.println("La suma es: "+ resultado);
+    }
+    
+}
