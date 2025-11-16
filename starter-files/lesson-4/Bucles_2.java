a73160600b21f350d29487ddfcfa0fdd36e61461
Carbocnito5443 <oscarleq@gmail.com>
2025-11-16 09:30:26 -0500

    fead: add code leccion 4

---
+++ starter-files/lesson-4/Bucles_2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       
+       int contador = 40;
+       
+       while(contador >= 0){
+           System.out.println("Cuenta: " + contador);
+           contador--;
+       }
+       System.out.println("VAMOS");
+    }
+}
+        
