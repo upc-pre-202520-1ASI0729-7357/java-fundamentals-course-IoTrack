71225803444270642e7eda7d7987b7a4c3f35262
Carbocnito5443 <oscarleq@gmail.com>
2025-11-16 09:28:08 -0500

    feat: add code leccion 3

---
+++ starter-files/lesson-3/Condicionales.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+      int edad = 18;
+      
+      if(edad>=18){
+          System.out.println("Eres mayor de edad");
+      } else{
+          System.out.println("Eres menor de edad");
+      }
+
+    }
+}
