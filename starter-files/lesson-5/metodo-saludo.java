3a86d39706825bd50d29a4f973b3cf2657d2a86d
nOOmzzzz <123845160+nOOmzzzz@users.noreply.github.com>
2025-11-16 03:20:12 -0500

    Add greeting method in metodo-saludo.java

---
+++ starter-files/lesson-5/metodo-saludo.java
@@ -0,0 +1,16 @@
+//Bienvenidos al curso de metodos en JAVA
+//metodo sumar() = cuatro
+//metodo restar()
+//metodo multipicar()
+//metodo dividir()
+//metodo int, void, string
+public class Main {
+    public static void saludar(String nombre){
+        System.out.println("Hola " + nombre + ", bienvenidos a nuestro curso de JAVA :)");
+    }
+    
+    public static void main(String[] args){
+        saludar("Axel");
+    }
+    
+}
