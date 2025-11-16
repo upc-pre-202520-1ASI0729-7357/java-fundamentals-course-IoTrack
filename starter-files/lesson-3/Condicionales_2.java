1236ceec59fc129058fe4042e7ae2f685b831946
Carbocnito5443 <oscarleq@gmail.com>
2025-11-16 09:29:16 -0500

    feat: add codigo leccion 3

---
+++ starter-files/lesson-3/Condicionales_2.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 100;
+        int b = 100;
+        
+        if(a > b){
+            System.out.println("A es mayor a B");
+        }else if (a < b){
+            System.out.println("B es mayor a A");
+        }else{
+            System.out.println("A y B son iguales");
+        }
+        
+
+    }
+}
